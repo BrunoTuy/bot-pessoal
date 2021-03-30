dba6edcd6340328b299d7c48bd2c6afa6646e919
Bruno Tuy <murta@tuy.com.br>
2021-03-30 08:04:52 -0300

    não executando os comandos do ivecao

--- comandos/index.js
+++ comandos/index.js
@@ -4,7 +4,7 @@ const list = {};
 
 fs
   .readdirSync(__dirname)
-  .filter( ( file )  => (file.indexOf(".") !== 0) && (file !== "index.js") )
+  .filter( ( file )  => (file.indexOf(".") !== 0) && !["index.js", "ivecao.js"].includes(file) )
   .forEach( ( file ) => {
     const obj = require(path.join(__dirname, file));
     const name = file.includes('.js') ? file.substring( 0, file.length-3 ) : file;
