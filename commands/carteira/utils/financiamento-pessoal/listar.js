5188f5fc6fd235bdf9923a2fa01e7e8a69f0b383
Bruno Tuy <murta@tuy.com.br>
2021-09-09 08:33:08 -0300

    status na lista de fp

--- commands/carteira/utils/financiamento-pessoal/listar.js
+++ commands/carteira/utils/financiamento-pessoal/listar.js
@@ -10,10 +10,18 @@ const exec = async ({ callback, lib, libLocal }) => {
     const debitosTotal = debitos.reduce((a, { valor }) => a + numeroPositivo(valor), 0);
     const creditosTotal = creditos.reduce((a, { valor }) => a + numeroPositivo(valor), 0);
     const saldo = -1 * debitosTotal + creditosTotal;
+    const creditoPendente = creditos.reduce((a, { valor, conta, status }) => a + (conta !== 'dm' || status !== 'feito' ? 0 : numeroPositivo(valor)) );
+    const creditoFeito = creditos.reduce((a, { valor, conta, status }) => a + (conta === 'dm' || status === 'feito' ? numeroPositivo(valor) : 0) );
+
+    const status = creditoPendente !== 0 && creditoPendente+creditoFeito-debitosTotal === 0
+      ? '🗓'
+      : creditoFeito === debitosTotal
+        ? '✅'
+        : '❌';
 
     total += saldo;
 
-    linhas.push(`${descritivo} | Deb ${debitos.length} $ ${libLocal.formatReal(debitosTotal)} | Cre ${creditos.length} $ ${libLocal.formatReal(creditosTotal)} | Saldo ${libLocal.formatReal(saldo)}`);
+    linhas.push(`${status} ${descritivo} | D${debitos.length}$${libLocal.formatReal(debitosTotal)} | C${creditos.length}$${libLocal.formatReal(creditosTotal)} | S$${libLocal.formatReal(saldo)}`);
   });
 
   linhas.push('');
