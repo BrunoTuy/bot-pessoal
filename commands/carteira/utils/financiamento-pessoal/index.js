b457f53ec7055c69b26d08669a88b9ac526dc1bc
Bruno Tuy <bruno.tuy@pixeon.com>
2021-12-18 08:56:56 -0300

    Ajustes no nome e descricao de financiamento pessoal

--- commands/carteira/utils/financiamento-pessoal/index.js
+++ commands/carteira/utils/financiamento-pessoal/index.js
@@ -2,8 +2,8 @@ const getStruct = require('../../../../lib/pathIndex');
 
 module.exports = getStruct({
   dir: __dirname,
-  alias: ['fp'],
-  label: '-- Financiamento pessoal tools --',
+  alias: ['tf', 'fp'],
+  label: '-- Tuy financeira tools --',
   hasContext: true,
   restricted: false,
   description: 'Financiamento pessoal'
